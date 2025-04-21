4800ccfd8426d9b226f0ddf47ec50c9933a21148
Nguyễn Quang Sang <55171560+ngwgsang@users.noreply.github.com>
2025-04-21 11:30:01 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='toolkit',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+    ],
+    author='Sang Quang Nguyen',
+    description='A reusable NLP research toolkit with some bug',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
