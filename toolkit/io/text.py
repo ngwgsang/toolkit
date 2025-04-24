b5c7a359559a099320eb76f7366f205bc2cee6c4
Nguyễn Quang Sang <55171560+ngwgsang@users.noreply.github.com>
2025-04-24 13:45:54 +0700

    up

--- toolkit/io/text.py
+++ toolkit/io/text.py
@@ -4,12 +4,32 @@ def clean_text(text):
     return re.sub(r"\s+", " ", text.strip())
 
 def split_sentences(text):
-    # Rất đơn giản, có thể thay bằng nltk hoặc underthesea nếu cần
     return re.split(r"(?<=[.!?]) +", text)
 
 def is_ascii(text):
     return all(ord(c) < 128 for c in text)
 
-def is_number(text):
-    return type(text) == type(79)
+def remove_chars(
+    text, 
+    blacklist=None, 
+    whitelist=(
+        "aăâbcdđeêghiklmnoôơpqrstuưvxy"
+        "AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXY"
+        "áàảãạắằẳẵặấầẩẫậéèẻẽẹếềểễệ"
+        "íìỉĩịóòỏõọốồổỗộớờởỡợúùủũụ"
+        "ứừửữựýỳỷỹỵÁÀẢÃẠẮẰẲẴẶẤẦẨẪẬ"
+        "ÉÈẺẼẸẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌỐỒỔỖỘ"
+        "ỚỜỞỠỢÚÙỦŨỤỨỪỬỮỰÝỲỶỸỴ"
+        "0123456789 "
+    )):
+    if whitelist is not None:
+        return ''.join(char for char in text if char in whitelist)
+    elif blacklist is not None:
+        return ''.join(char for char in text if char not in blacklist)
+    else:
+        return text
 
+def remove_sw(text, sw):
+    words = text.split()
+    filtered = [word for word in words if word.lower() not in sw]
+    return ' '.join(filtered)
